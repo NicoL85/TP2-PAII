d4dc22476bc266ae9d5ce8a5a9cfc183596bf329
NicoL85 <82108174+NicoL85@users.noreply.github.com>
2023-09-04 19:57:21 -0300

    Ej1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Agregacion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int result;
+
+        static int Sumar(int numero1, int numero2)
+        {
+
+            return numero1 + numero2;
+
+        }
+        result = Sumar(10, 8);
+
+        Console.WriteLine("resultado" + result);
+
+
+    }
+}
+
