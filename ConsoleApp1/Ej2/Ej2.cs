c2a06d00ac29783784654b738c8af8e55008e50c
NicoL85 <82108174+NicoL85@users.noreply.github.com>
2023-09-04 20:03:26 -0300

    update Ej2

---
+++ ConsoleApp1/Ej2/Ej2.cs
@@ -0,0 +1,29 @@
+﻿
+//namespace Agregacion;
+class Ej2
+{
+    static void Main(string[] args)
+    {
+
+
+        string cadenas;
+
+
+        static string Cortar(string cadena)
+        {
+            if (cadena.Length >= 4)
+            {
+                cadena = cadena.Substring(0, 4);
+
+            }
+            return cadena;
+
+        }
+        cadenas = Cortar("HolaMundo");
+        Console.WriteLine("Cortar cadena : " + cadenas);
+
+
+
+    }
+
+}
